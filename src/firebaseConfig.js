365e8c6ad088ad5d3f60490886036c450d0bf6be
YoonHyeJin <emfkdlvnem@gmail.com>
2023-10-08 21:04:57 +0900

    firebase setting

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,15 +2,27 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-export const firebaseConfig = {
-    apiKey: 'AIzaSyBfXC9gasc0NI7bFx-BYN52ULFMjU9LLho',
-    authDomain: 'koita-1c079.firebaseapp.com',
-    databaseURL: 'https://koita-1c079-default-rtdb.asia-southeast1.firebasedatabase.app',
-    projectId: 'koita-1c079',
-    storageBucket: 'koita-1c079.appspot.com',
-    messagingSenderId: '240009170899',
-    appId: '1:240009170899:web:a7184866ddf02275e56926',
-    measurementId: 'G-NHQ6QB7LNY',
+// 이전 저장소 정보
+// export const firebaseConfig = {
+//     apiKey: 'AIzaSyBfXC9gasc0NI7bFx-BYN52ULFMjU9LLho',
+//     authDomain: 'koita-1c079.firebaseapp.com',
+//     databaseURL: 'https://koita-1c079-default-rtdb.asia-southeast1.firebasedatabase.app',
+//     projectId: 'koita-1c079',
+//     storageBucket: 'koita-1c079.appspot.com',
+//     messagingSenderId: '240009170899',
+//     appId: '1:240009170899:web:a7184866ddf02275e56926',
+//     measurementId: 'G-NHQ6QB7LNY',
+// };
+
+// 새로운 저장소 정보
+const firebaseConfig = {
+    apiKey: 'AIzaSyA8f6BwPlVIDJuUHvb4xW2Q-tfRBCcoiJk',
+    authDomain: 'reactkoita.firebaseapp.com',
+    projectId: 'reactkoita',
+    storageBucket: 'reactkoita.appspot.com',
+    messagingSenderId: '1058597611566',
+    appId: '1:1058597611566:web:ece23b06e5b45b8fea17bf',
+    measurementId: 'G-TQ4PKBSNGG',
 };
 
 const app = initializeApp(firebaseConfig);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
     const [success, setSuccess] = useState('');
 
     const navigate = useNavigate(); //네비게이트훅
-    
+
     // 일반 로그인
     const handleLogin = async (e) => {
         e.preventDefault();
@@ -23,7 +23,7 @@ function Login() {
             await signInWithEmailAndPassword(auth, email, password);
             setSuccess('로그인에 성공하였습니다. 메인 페이지로 이동합니다.');
             setTimeout(() => {
-                navigate('/'); // 로그인 성공 하면 메인페이지로 
+                navigate('/'); // 로그인 성공 하면 메인페이지로
             }, 2000);
         } catch (error) {
             if (error.code === 'auth/user-not-found') {
@@ -50,49 +50,65 @@ function Login() {
             setError('Google 로그인에 실패했습니다.');
         }
     };
-    
+
     return (
         <section className="login-area relative max-w-set mt-37.5 mx-auto pt-4 px-6 pb-25">
             <h2 className="section-title flex flex-col justify-center items-center">
                 <span className="block text-40 mb-1">WELCOME!</span>
-                <img src={logo} alt="logo image" className="w-30"/>
+                <img src={logo} alt="logo image" className="w-30" />
             </h2>
             <div className="login-box relative w-112.5 h-125 px-12 py-7 mx-auto my-12 rounded-lg shadow-custom">
                 <div className="social-area my-5 mx-auto text-center">
-                    <button type="button" onClick={handleGoogleLogin} className="relative w-87.5 h-12.5 shadow-md bg-white ">
+                    <button
+                        type="button"
+                        onClick={handleGoogleLogin}
+                        className="relative w-87.5 h-12.5 shadow-md bg-white "
+                    >
                         <span>구글 로그인</span>
                     </button>
                 </div>
                 <p className="txt relative text-center my-12 mx-auto">또는</p>
                 <form className="login-form w-87.5 mt-5 mx-auto mb-10" onSubmit={handleLogin}>
                     <div>
-                        <input 
-                            type="email" 
-                            id="email-login" 
+                        <input
+                            type="email"
+                            id="email-login"
                             placeholder="koita에 가입된 이메일 주소"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
-                            className="w-87.5 h-12.5 p-4.5 mb-5 rounded-lg shadow-md "    
+                            className="w-87.5 h-12.5 p-4.5 mb-5 rounded-lg shadow-md "
                         />
                     </div>
                     <div>
-                        <input 
-                            type="password" 
-                            id="password-login" 
-                            placeholder="비밀번호" 
+                        <input
+                            type="password"
+                            id="password-login"
+                            placeholder="비밀번호"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             className="w-87.5 h-12.5 p-4.5 mb-5 rounded-lg shadow-md"
                         />
                     </div>
-                    <button type="submit" id="btn-login" className="w-87.5 h-12.5 p-4.5 mb-5 text-white font-bold rounded-lg shadow-md bg-set-brown">로그인</button>
+                    <button
+                        type="submit"
+                        id="btn-login"
+                        className="w-87.5 h-12.5 p-4.5 mb-5 text-white font-bold rounded-lg shadow-md bg-set-brown"
+                    >
+                        로그인
+                    </button>
                 </form>
                 {success && <p className="success">{success}</p>}
                 {error && <p className="error">{error}</p>}
-                <Link to="/signup" id="btn-signup" className="btn btn-signup-move absolute right-14 bottom-8 py-2 px-3 rounded-lg"><span>회원가입</span></Link>
+                <Link
+                    to="/signup"
+                    id="btn-signup"
+                    className="btn btn-signup-move absolute right-14 bottom-8 py-2 px-3 rounded-lg"
+                >
+                    <span>회원가입</span>
+                </Link>
             </div>
         </section>
-    )
+    );
 }
 
 export default Login;
